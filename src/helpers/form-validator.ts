bd08835fde754a394d05e9ec1f7f1e0114fdf303
moises-herrera <moisesherrera037@gmail.com>
2023-04-26 19:48:08 -0500

    refactor: form validation method

--- src/helpers/form-validator.ts
+++ src/helpers/form-validator.ts
@@ -3,32 +3,36 @@ import { FormGroup } from '@angular/forms';
 /** Represents all the common methods to validate a form. */
 export class FormValidator {
   /** Pattern to validate an email. */
-  static readonly emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,7}$';
+  static readonly emailPattern = '^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,7}$';
 
   /** Pattern to validate a password. */
-  static readonly passwordPattern = '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.* ).{6,}$';
+  static readonly passwordPattern =
+    '^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?!.* ).{6,}$';
 
   /**
-   * Validate that the fields are equal.
+   * Validate that the password fields are equal.
    *
    * @param fieldName Field name to evaluate.
    * @param secondFieldName Second field name.
    * @returns A function to validate a form group.
    */
-  static validateEqualFields(
+  static validatePasswordsMatch(
     fieldName: string,
     secondFieldName: string
   ): (formGroup: FormGroup) => void {
     return (formGroup: FormGroup): void => {
       const formControl = formGroup.get(fieldName);
       const secondFormControl = formGroup.get(secondFieldName);
-      const areFieldsValid = formControl?.value === secondFormControl?.value;
+      const areFieldsValid =
+        formControl &&
+        secondFormControl &&
+        formControl.value === secondFormControl.value;
 
       if (areFieldsValid) {
         secondFormControl?.setErrors(null);
       } else {
-        secondFormControl?.setErrors({ fieldsAreNotEqual: true });
+        secondFormControl?.setErrors({ passwordsMatch: true });
       }
     };
-  };
+  }
 }
