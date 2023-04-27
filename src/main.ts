dcb96195b260b5eaa11ab55313beda141a906888
moises-herrera <moisesherrera037@gmail.com>
2023-04-26 19:47:50 -0500

    deps: add error tailor

--- src/main.ts
+++ src/main.ts
@@ -6,14 +6,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { importProvidersFrom } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
-
+import { provideErrorTailorConfig } from '@ngneat/error-tailor';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(appRoutes),
     importProvidersFrom(BrowserAnimationsModule),
     importProvidersFrom(HttpClientModule),
-    MessageService
-]
-})
-  .catch(err => console.error(err));
+    provideErrorTailorConfig({
+      errors: {
+        useValue: {
+          required: 'Este campo es requerido',
+          minlength: ({ requiredLength }) => `El campo debe tener mínimo ${requiredLength} carácteres`,
+          pattern: 'El valor ingresado es inválido',
+          passwordsMatch: 'Las contraseñas no coinciden'
+        },
+      },
+    }),
+    MessageService,
+  ],
+}).catch((err) => console.error(err));
