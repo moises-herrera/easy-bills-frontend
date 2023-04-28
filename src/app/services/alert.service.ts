286f55ae5e28bb1dd769357859596557afc4f06e
moises-herrera <moisesherrera037@gmail.com>
2023-04-28 16:06:59 -0500

    feat: display confirm dialog

--- src/app/services/alert.service.ts
+++ src/app/services/alert.service.ts
@@ -1,13 +1,16 @@
 import { Injectable, inject } from '@angular/core';
-import { Message, MessageService } from 'primeng/api';
+import { ConfirmationService, Message, MessageService } from 'primeng/api';
 
 /**
  * Alert service.
  */
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AlertService {
+  /** Confirmation service to manage confirm dialog. */
+  private _confirmationService = inject(ConfirmationService);
+
   /** Message service. */
   private _messageService = inject(MessageService);
 
@@ -18,5 +21,21 @@ export class AlertService {
    */
   displayMessage(message: Message): void {
     this._messageService.add(message);
+  }
+
+  /**
+   * Display confirmation dialog.
+   *
+   * @param acceptAction Function called when the accept event is triggered.
+   * @param rejectAction Function called when the reject event is triggered.
+   */
+  displayConfirm(acceptAction: () => void, rejectAction?: () => void): void {
+    this._confirmationService.confirm({
+      message: '¿Estás seguro?',
+      header: 'Confirmación',
+      icon: 'pi pi-exclamation-triangle',
+      accept: acceptAction,
+      reject: rejectAction,
+    });
   }
 }
