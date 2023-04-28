9d1a3f060f30801ada9ec2216f5cf2e9ecf22d29
moises-herrera <moisesherrera037@gmail.com>
2023-04-27 22:30:10 -0500

    feat: add categories page

--- src/app/pages/home/categories/categories.component.ts
+++ src/app/pages/home/categories/categories.component.ts
@@ -1,13 +1,61 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+import { InputTextModule } from 'primeng/inputtext';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { CategoryService } from 'src/app/services/category.service';
+import { Category } from 'src/models';
+import { Observable, map, tap } from 'rxjs';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
+    DividerModule,
+    ProgressSpinnerModule,
+  ],
   templateUrl: './categories.component.html',
-  styleUrls: ['./categories.component.css']
+  styleUrls: ['./categories.component.css'],
 })
 export class CategoriesComponent {
+  /** Category service. */
+  private _categoryService = inject(CategoryService);
 
+  /** Search text. */
+  search = '';
+
+  /** List of categories. */
+  categories$!: Observable<Category[]>;
+
+  /** If the data is loading. */
+  isLoading = false;
+
+  /**
+   * Initial life cycle method.
+   */
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.categories$ = this._categoryService
+      .getCategories()
+      .pipe(tap(() => (this.isLoading = false)));
+  }
+
+  /**
+   * Search categories.
+   */
+  searchCategories(): void {
+    this.categories$ = this.categories$.pipe(
+      map((categories) =>
+        categories.filter(({ name }) =>
+          name.toLowerCase().includes(this.search.toLowerCase())
+        )
+      )
+    );
+  }
 }
