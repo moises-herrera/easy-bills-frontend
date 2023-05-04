d6d19fb966fdd64cffb406648352b6fa58b9c1ed
moises-herrera <moisesherrera037@gmail.com>
2023-05-03 19:03:51 -0500

    refactor: mask of password input

--- src/app/pages/auth/register/register.component.ts
+++ src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {
   AbstractControl,
