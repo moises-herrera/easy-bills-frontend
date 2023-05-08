bd2dc9e7bf3188c1695aaf9bbfc32b4b67ac130b
moises-herrera <moisesherrera037@gmail.com>
2023-05-07 21:16:05 -0500

    chore: update base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  baseUrl: 'appeasybills.azurewebsites.net/api',
+  baseUrl: 'https://appeasybills.azurewebsites.net//api',
   accessTokenLifetime: 86400000
 }
