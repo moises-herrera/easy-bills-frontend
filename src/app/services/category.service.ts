41176bdc231a1a10bd7e7c2547444dbe0b818fe4
moises-herrera <moisesherrera037@gmail.com>
2023-05-16 21:55:46 -0500

    feat: add pagination to get categories

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -1,8 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable, shareReplay } from 'rxjs';
 import { environment } from 'src/environments/environment';
-import { Category } from 'src/models';
+import { Category, PagedResponse } from 'src/models';
 
 const baseUrl = `${environment.baseUrl}/categories`;
 
@@ -19,11 +19,20 @@ export class CategoryService {
   /**
    * Get all the categories.
    *
+   * @param to The to date.
+   * @param pageNumber The page number.
    * @returns List of categories.
    */
-  getCategories(): Observable<Category[]> {
+  getCategories(
+    pageNumber: number = 1,
+    pageSize: number = 10
+  ): Observable<PagedResponse<Category>> {
+    const params = new HttpParams()
+      .set('pageNumber', pageNumber)
+      .set('pageSize', pageSize);
+
     return this._http
-      .get<Category[]>(baseUrl)
+      .get<PagedResponse<Category>>(baseUrl, { params })
       .pipe(shareReplay({ bufferSize: 1, refCount: true }));
   }
 
@@ -58,7 +67,10 @@ export class CategoryService {
    * @param category The category data.
    * @returns An empty observable.
    */
-  updateCategory(categoryId: string, category: Partial<Category>): Observable<void> {
+  updateCategory(
+    categoryId: string,
+    category: Partial<Category>
+  ): Observable<void> {
     return this._http
       .put<void>(`${baseUrl}/${categoryId}`, category)
       .pipe(shareReplay({ bufferSize: 1, refCount: true }));
