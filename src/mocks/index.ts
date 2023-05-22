a602a9a23b6b610715a97a6d44b5bc36691494df
moises-herrera <moisesherrera037@gmail.com>
2023-05-22 17:24:22 -0500

    chore: add mocks index

---
+++ src/mocks/index.ts
@@ -0,0 +1,6 @@
+export * from './mock-account-service';
+export * from './mock-category-service';
+export * from './mock-transaction-service';
+export * from './mock-user-service';
+export * from './mock-alert-service';
+export * from './mock-error-tailor-config';
