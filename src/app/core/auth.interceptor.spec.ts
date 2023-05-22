52c7d488d45f4a4466107973b924acc5c0112ae4
moises-herrera <moisesherrera037@gmail.com>
2023-05-22 17:57:12 -0500

    chore: config auth interceptor tests

--- src/app/core/auth.interceptor.spec.ts
+++ src/app/core/auth.interceptor.spec.ts
@@ -1,13 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthInterceptor } from './auth.interceptor';
+import { UserService } from 'src/app/services/user.service';
+import { MockUserService } from 'src/mocks';
 
 describe('AuthInterceptor', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    providers: [
-      AuthInterceptor
-      ]
-  }));
+  beforeEach(() =>
+    TestBed.configureTestingModule({
+      providers: [
+        AuthInterceptor,
+        { provide: UserService, useClass: MockUserService },
+      ],
+    })
+  );
 
   it('should be created', () => {
     const interceptor: AuthInterceptor = TestBed.inject(AuthInterceptor);
