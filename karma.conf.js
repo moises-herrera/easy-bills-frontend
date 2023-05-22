a1fa4ad3827ac137e5a88718621be4e1a5501a5d
moises-herrera <moisesherrera037@gmail.com>
2023-05-22 18:47:51 -0500

    refactor: update karma.conf

--- karma.conf.js
+++ karma.conf.js
@@ -33,14 +33,14 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ["Chrome"],
+    browsers: ["Chrome", "ChromeHeadless", "ChromeHeadlessCI"],
     browserConsoleLogOptions: {
       level: "log",
     },
     customLaunchers: {
       ChromeHeadlessCI: {
         base: "ChromeHeadless",
-        flags: ["--no-sandbox", "--disable-gpu"],
+        flags: ["--no-sandbox"],
       },
     },
   });
