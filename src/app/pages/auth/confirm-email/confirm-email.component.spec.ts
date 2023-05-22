be1c03285d5dfb3fc1a1e92af18eecb28b9d670a
moises-herrera <moisesherrera037@gmail.com>
2023-05-22 17:26:11 -0500

    chore: config auth tests

--- src/app/pages/auth/confirm-email/confirm-email.component.spec.ts
+++ src/app/pages/auth/confirm-email/confirm-email.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConfirmEmailComponent } from './confirm-email.component';
+import { AlertService } from 'src/app/services/alert.service';
+import { UserService } from 'src/app/services/user.service';
+import { MockUserService, MockAlertService } from 'src/mocks';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('ConfirmEmailComponent', () => {
   let component: ConfirmEmailComponent;
@@ -8,9 +12,18 @@ describe('ConfirmEmailComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ ConfirmEmailComponent ]
-    })
-    .compileComponents();
+      imports: [ConfirmEmailComponent, RouterTestingModule],
+      providers: [
+        {
+          provide: UserService,
+          useClass: MockUserService,
+        },
+        {
+          provide: AlertService,
+          useClass: MockAlertService,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ConfirmEmailComponent);
     component = fixture.componentInstance;
